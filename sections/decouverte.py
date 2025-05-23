086419ea4e816d0f39e17b5e34abb9d6135a0b66
Celine Ohl <celine.ohl@gmail.com>
2025-05-23 17:38:37 +0200

    Realization of data visualisation part

--- sections/decouverte.py
+++ sections/decouverte.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from utils.data_loader import load_data
 
 def show_decouverte():
@@ -65,24 +67,18 @@ def show_decouverte():
         st.subheader(f"Distribution de la variable : {selected_cat} ({nb_cat} modalités possibles)")
         st.dataframe(df_stats)
 
-        st.markdown("""
-        ### Remarques générales
-
-        > On remarque que les variables ont le plus souvent entre 2 et 4 modalités.  
-        > Sauf pour la variable `"job"` et évidemment les mois.  
+        st.subheader("Remarques générales")
+        if st.checkbox("Afficher les remarques générales sur les variables catégorielles", value = True):
+            st.markdown("""
+        - On remarque que les variables ont le plus souvent entre 2 et 4 modalités. Sauf pour la variable `job` et évidemment les mois.
+        - La variable `job` contient 12 modalités différentes, ce qui est relativement élevé. On pourrait donc envisager de regrouper certaines modalités pour simplifier l'analyse.
         
-        > On peut aussi noter l’apparition de la valeur `"unknown"` pour certaines variables : `job`, `education`, `contact` et `poutcome`.  
-        > Ce qui laisse sous-entendre que même si on n’avait pas de valeurs nulles dans le dataset, il y a tout de même des données manquantes pour certaines catégories.  
+        - On peut aussi noter l’apparition de la valeur `unknown` pour certaines variables : `job`, `education`, `contact` et `poutcome`.  Ce qui laisse sous-entendre que même si on n’avait pas de valeurs nulles dans le dataset, il y a tout de même des données manquantes pour certaines catégories.  
                     
-        > Dans certains cas, l’impact est faible car peu de données manquantes,  
-        > mais pour d’autres variables comme `"poutcome"`, la majorité des données sont `"unknown"`.  
-        > Il faudra donc garder ces informations en tête pour la suite du projet et décider quoi faire avec ces variables et données manquantes  
-        > (exemple : réduire le dataset).  
+        - Dans certains cas, l’impact est faible car peu de données manquantes, mais pour d’autres variables comme `poutcome`, la majorité des données sont `unknown`.  
+          Il faudra donc garder ces informations en tête pour la suite du projet et décider quoi faire avec ces variables et données manquantes (exemple : réduire le dataset).  
 
-        > Certaines variables contiennent les valeurs `"no"` et `"yes"`.  
-        > Comme ces variables ne contiennent pas de valeurs manquantes,  
-        > il peut être judicieux de les transformer en type booléen.  
-        > Cela permettra d’optimiser les calculs, la mémoire et la manipulation des données.  
+        - Certaines variables contiennent les valeurs `no` et `yes`. Comme ces variables ne contiennent pas de valeurs manquantes, il peut être judicieux de les transformer en type booléen. Cela permettra d’optimiser les calculs, la mémoire et la manipulation des données.  
         """, unsafe_allow_html = True)
 
         st.subheader("Détail des variables catégorielles")
@@ -131,7 +127,7 @@ def show_decouverte():
             - Ce qui en fait une donnée peu pertinente dans la suite des analyses  
         <br>
         - `deposit`  
-            - C'est la valeur cible qui nous intéresse et qui nous dit si la campagne marketing a été un succès (le client a souscrit au compte à terme : yes) ou non (no)  
+            - C'est la valeur cible qui nous intéresse et qui nous dit si la campagne marketing a été un succès (le client a souscrit au compte à terme : yes ou no)  
             - Pas de valeurs manquantes  
             - C'est donc cette variable qui sera principalement utilisée dans les analyses bivariées  
             - On voit qu'il n'y a pas de déséquilibre (52% vs 47 %) et donc potentiellement pas de besoin de rééchantillonnage
@@ -143,3 +139,95 @@ def show_decouverte():
         st.subheader("Statistiques descriptives")
         st.dataframe(df.describe().T)
         st.markdown("---")
+
+        # Stockage des variables numériques 
+        var_num = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
+
+        st.subheader("Analyse des outliers (valeurs extrêmes)")
+        # Calcul des outliers et stockage des résultats
+        outlier_data = []
+
+        for col in var_num:
+            Q1 = df[col].quantile(0.25)
+            Q3 = df[col].quantile(0.75)
+            IQR = Q3 - Q1
+            limite_inf = Q1 - 1.5 * IQR
+            limite_sup = Q3 + 1.5 * IQR
+            outliers = df[(df[col] < limite_inf) | (df[col] > limite_sup)][col]
+            pourcentage = len(outliers) / len(df) * 100
+            outlier_data.append({
+                'Variable': col,
+                'Pourcentage d\'outliers': round(pourcentage, 2)
+            })
+
+        # Création du DataFrame des résultats
+        df_outliers = pd.DataFrame(outlier_data)
+
+        # Affichage du df
+        st.dataframe(df_outliers)
+
+
+        st.subheader("Affichage de la distribution des variables numériques")
+        # Création de la figure
+        fig, axs = plt.subplots(2, 4, figsize = (16, 8))  # 2 lignes, 4 colonnes
+
+        # Création des boxplots
+        for i, col in enumerate(var_num):
+            ax = axs[i // 4, i % 4]
+            sns.boxplot(x = 'deposit', y = col, data = df, ax = ax)
+            ax.set_title(f'Distribution de {col} selon deposit')
+            ax.tick_params(axis = 'x', rotation = 45)
+
+        plt.tight_layout()
+        # Affichage dans Streamlit
+        st.pyplot(fig)
+        
+
+        st.subheader("Remarques générales")
+        if st.checkbox("Afficher les remarques générales sur les variables numériques", value = True):
+            st.markdown("""
+        - On peut noter des valeurs négatives pour certaines variables. Dans le cas du solde par exemple, cela est cohérent
+            - Pour les autres variables, il semble que -1 signifie que les données sont manquantes.
+        - L'écart type de certaines variables est très élevé. Cela indique une grande disperssion et une moyenne potentiellement tirée vers le haut par des valeurs extrêmes. 
+        - Le traitement des outliers devra être considéré avec attention, particulièrement pour pdays, previous et balance
+        """, unsafe_allow_html = True)
+
+
+        st.subheader("Détail des variables numériques")
+        if st.checkbox("Afficher les observations sur les variables numériques", value = True):
+            st.markdown("""
+        - `age`
+            - **Age du client**
+            - La moyenne est de 41 ans
+            - Le client le plus jeune a 18 ans
+            - Le client le plus âgé à 95 ans
+            - 50% des clients ont entre 32 et 39 ans
+        - `balance`
+            - **Solde du compte**
+            - Il y a une grande disperssion des données avec un min négatif et un max très important
+            - 75% des clients ont moins de 1708 dollars sur leurs comptes
+            - Le max a 81 204 dollars impacte la moyenne qui est 3 fois supérieure à la médiane
+        - `day`
+            - **jour du dernier contact**
+        - `duration`
+            - **Durée du dernier contact en seconde**
+            - La moyenne d'un contact est de 372 secondes, soit environ 6 minutes
+            - 50% des appels durent entre 138 et 496 secondes (2 à 8 minutes)
+            - On voit là aussi des valeurs très importantes avec un max à 3881 secondes soit un appel de plus d'une heure
+        - `campaign`
+            - **Nombre de contact avec le client durant la campagne**
+            - En moyenne les clients sont contactés 2,5 fois
+            - 75% des clients sont contactés 3 fois ou moins
+            - On sait donc rapidement si un client va décider de souscrire ou non
+        - `pdays`
+            - **Nombre de jours depuis le dernier contact avec le client depuis la précédente campagne**
+            - La valeur -1 indique que le client n'a pas été contacté précédemment ou que la valeur est manquante
+            - Plus de 50% des clients n'ont pas été contactés lors d'une précédente campagne
+        - `previous`
+            - **Nombre de contact précédent la campagne en cours**
+            - La valeur 0 indique que le client n'a pas été contacté précédemment
+            - 75% des clients ont été contacté une fois ou moins
+            - On peut déduire que la nouvelle campagne cherche à toucher de nouveaux prospects
+            - On a aussi des valeurs extrêmes où un client a été contacté 58 fois
+            - Peut être que le groupe de client déjà contacté a toujours souscrit aux offres précédentes et serait donc une bonne cible stratégique pour la nouvelle campagne. Chose que l'on pourra vérifier lors des analyses graphiques
+        """, unsafe_allow_html = True)
--- sections/exploration.py
+++ sections/exploration.py
@@ -1,9 +1,135 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from utils.data_loader import load_data
+import textwrap
+import inspect
 
 def show_exploration():
+    # Chargement des données
+    df = load_data()
+
+    # Préparation des données numériques
+    def prepa_num(df) :
+        balance_mediane = df['balance'].median()
+
+        df['balance_group'] = df['balance'].apply(lambda x: '<0' if x < 0
+                                                else f'0-{int(balance_mediane)}' if x <= balance_mediane
+                                                else f'>{int(balance_mediane)}')
+
+        df['age_group'] = pd.cut(df['age'], bins = [18, 32, 38, 48, 95], labels = ['18-32', '33-38', '39-48', '49-95'])
+
+        balance_order = pd.CategoricalDtype(categories = ['<0', '0-550', '>550'], ordered = True)
+        df['balance_group'] = df['balance_group'].astype(balance_order)
+        return df
+
+    # Préparation des données catégorielles
+    def prepa_cat(df) : 
+        df['month'] = pd.Categorical(df['month'],
+                                    categories = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 
+                                                    'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+                                    ordered = True)
+
+        job_mapping = {
+            'management': 'Management',
+            'entrepreneur': 'Independant', 'self-employed': 'Independant',
+            'housemaid': 'Services', 'services': 'Services',
+            'technician': 'Ouvriers-Techniciens', 'admin.': 'Ouvriers-Techniciens', 'blue-collar': 'Ouvriers-Techniciens',
+            'retired': 'Autres', 'unemployed': 'Autres', 'student': 'Autres'
+        }
+        df['job_group'] = df['job'].map(job_mapping).fillna('Autres')
+        return df
+
+
     st.title("Exploration et visualisation des données")
-    tab1, tab2 = st.tabs(["Visualisations catégorielles", "Visualisations numériques"])
-    with tab1:
-        st.write("Graphiques des variables catégorielles")
-    with tab2:
-        st.write("Graphiques des variables numériques")
+    st.subheader("Préparation des données")
+    st.markdown("""
+    Dans la découverte des données, on a pu constater que la variable `job` a beaucoup de modalités. On va donc réduire le nombre
+                de modalités de cette variable en regroupant les modalités peu fréquentes dans une catégorie `autres`.
+                Et faire également des regroupements par type de métier.""")
+    
+    st.code(textwrap.dedent(inspect.getsource(prepa_cat)), language = 'python')
+    
+    st.markdown("""
+    Il en va de même pour la variable `age`que l'on va regrouper par tranche d'âge. <br>
+    Enfin, pour la `balance`, on va également créer des sous groupes pour éviter les valeurs extrêmes.
+    """, unsafe_allow_html = True)
+
+    st.code(textwrap.dedent(inspect.getsource(prepa_num)), language = 'python')
+    # Affichage variables numériques vs deposit
+    st.subheader("Comparaison des variables numériques avec la variable cible")
+
+
+    df = prepa_num(df)
+
+    age_percentages = pd.crosstab(df['age_group'], df['deposit'], normalize = 'index') * 100
+    balance_percentages = pd.crosstab(df['balance_group'], df['deposit'], normalize = 'index') * 100
+    day_percentages = pd.crosstab(df['day'], df['deposit'], normalize = 'index') * 100
+
+    fig, axes = plt.subplots(1, 3, figsize = (18, 5))
+
+    axes[0].bar(age_percentages.index, age_percentages['no'], color = '#A1C9F4', label = 'no')
+    axes[0].bar(age_percentages.index, age_percentages['yes'], bottom = age_percentages['no'],
+                color = '#FFB482', label = 'yes')
+    axes[0].set_title("Âge vs Deposit")
+    axes[0].set_xlabel("Tranche d'âge")
+    axes[0].set_ylabel("Pourcentage")
+    axes[0].legend(title = 'Deposit')
+    axes[0].grid(axis = 'y', linestyle = '--', alpha = 0.3)
+
+    axes[1].bar(balance_percentages.index, balance_percentages['no'], color = '#A1C9F4', label = 'no')
+    axes[1].bar(balance_percentages.index, balance_percentages['yes'], bottom = balance_percentages['no'],
+                color = '#FFB482', label = 'yes')
+    axes[1].set_title("Solde vs Deposit")
+    axes[1].set_xlabel("Groupe de solde")
+    axes[1].legend(title = 'Deposit')
+    axes[1].grid(axis = 'y', linestyle = '--', alpha = 0.3)
+
+    axes[2].bar(day_percentages.index, day_percentages['no'], color = '#A1C9F4', label = 'no')
+    axes[2].bar(day_percentages.index, day_percentages['yes'], bottom = day_percentages['no'],
+                color = '#FFB482', label = 'yes')
+    axes[2].set_title("Jour du mois vs Deposit")
+    axes[2].set_xlabel("Jour du mois")
+    axes[2].legend(title = 'Deposit')
+    axes[2].grid(axis = 'y', linestyle = '--', alpha = 0.3)
+    axes[2].set_xticks(range(1, 32))
+    axes[2].set_xticklabels(range(1, 32), fontsize = 8)
+
+    st.pyplot(fig)
+
+
+
+    # Affichage des variables catégorielles vs deposit
+    pastel_colors = ["#A1C9F4", "#FFB482"]
+
+    groupes = {
+        "Données démographiques": ['job_group', 'marital', 'education'],
+        "Données financières": ['default', 'housing', 'loan'],
+        "Données campagne": ['contact', 'month', 'poutcome']} 
+    
+    df = prepa_cat(df)
+
+    st.subheader("Comparaison des variables catégorielles avec la variable cible")
+
+    for groupe, variables in groupes.items():
+        with st.expander(f"{groupe}"):
+            fig, axes = plt.subplots(nrows = 1, ncols = len(variables), figsize = (5 * len(variables), 5))
+            fig.suptitle(f"{groupe}", fontsize = 16, fontweight = 'bold')
+
+            # Si une seule variable, axes ne sera pas une liste => forcer en liste
+            if len(variables) == 1:
+                axes = [axes]
+
+            for i, var in enumerate(variables):
+                cross_tab = pd.crosstab(df[var], df['deposit'], normalize = 'index') * 100
+                cross_tab.plot(kind = 'bar', ax = axes[i], color = pastel_colors)
+                axes[i].set_title(f"{var} vs deposit")
+                axes[i].set_ylabel("Pourcentage")
+                axes[i].set_xlabel(var)
+                axes[i].legend(title = "Deposit")
+                axes[i].tick_params(axis = 'x', rotation = 90)
+
+            plt.tight_layout(rect = [0, 0, 1, 0.92])
+            st.pyplot(fig)
